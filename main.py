6b9a076b760803198a81c5d6d358efe731eefc6e
student06 <jakub.gola2@teb.edu.pl>
2024-12-16 14:12:21 +0100

    16.12.2024

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os.path
 import sys
-from os import write
 
 from PyQt6.QtWidgets import QDialog, QApplication
 
@@ -14,7 +14,24 @@ class MyForm(QDialog):
         self.ui.students.itemClicked.connect(self.students_fail)
         self.ui.failStudents.itemClicked.connect(self.student_unfail)
         self.ui.judgement.clicked.connect(self.judge)
+        self.ui.judgement.clicked.connect(self.defender)
+        self.ui.addStudent.clicked.connect(self.add)
+        self.read_student()
         self.show()
+
+    def read_student(self):
+        if os.path.exists('text.txt') and os.path.exists('zdajacy.txt'):
+            self.ui.students.clear()
+            self.ui.failStudents.clear()
+            with open('zdajacy.txt', 'r') as file:
+                students = file.read().splitlines()
+                for student in students:
+                    self.ui.students.addItems(student)
+
+            with open('text.txt', 'r') as file:
+                students = file.read().splitlines()
+                for student in students:
+                    self.ui.failStudents.addItems(student)
 
     def students_fail(self):
         items = self.ui.students.selectedItems()
@@ -31,6 +48,17 @@ class MyForm(QDialog):
             for i in range(self.ui.failStudents.count()):
                 file.write(self.ui.failStudents.item(i).text())
                 file.write('\n')
+
+    def defender(self):
+        with open('./zdajacy.txt', 'w') as file:
+            for i in range(self.ui.students.count()):
+                file.write(self.ui.students.item(i).text())
+                file.write('\n')
+
+    def add(self):
+        name = self.ui.name.text()
+        surname = self.ui.surname.text()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
